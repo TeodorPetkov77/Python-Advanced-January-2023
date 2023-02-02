acebe1d2e50feef0f60680313d6f864ffac349b2
RebelShark <tpetkov93@gmail.com>
2023-02-03 00:18:58 +0200

    x

--- Exercises/13_exercise_file_handling/03_file_manipulator.py
+++ Exercises/13_exercise_file_handling/03_file_manipulator.py
@@ -2,7 +2,8 @@ import os
 
 
 def file_manipulator(command_f: list):
-    file_name = command[1]
+    action = command_f[0]
+    file_name = command_f[1]
 
     def create():
         with open(file_name, 'w') as file:
@@ -11,7 +12,7 @@ def file_manipulator(command_f: list):
     def replace():
         try:
             with open(file_name, 'r') as file:
-                text = file.read().replace(command[2], command[3])
+                text = file.read().replace(command_f[2], command_f[3])
             with open(file_name, 'w') as file:
                 file.write(text)
         except FileNotFoundError:
@@ -19,22 +20,20 @@ def file_manipulator(command_f: list):
 
     def add():
         with open(file_name, 'a') as file:
-            file.write(f"{command[2]}\n")
+            file.write(f"{command_f[2]}\n")
 
     def delete():
         if os.path.exists(file_name):
             os.remove(file_name)
         else:
             print("An error occurred")
-
     commands = {
-        'Create': lambda x: create(),
-        'Add': lambda x: add(),
-        'Replace': lambda x: replace(),
-        'Delete': lambda x: delete()
+        'Create': create,
+        'Add': add,
+        'Replace': replace,
+        'Delete': delete
     }
-    return commands[command_f[0]]
-
+    commands[action]()
 
 
 command = input()
@@ -42,3 +41,34 @@ while command != 'End':
     command = command.split('-')
     file_manipulator(command)
     command = input()
+
+
+# Keep in mind that the file may not appear or disappear in the Project tab immediately upon executing a command. Open the folder and check there.
+
+
+# 3.	File Manipulator
+# Create a program that will receive commands until the command "End". The commands can be:
+# •	"Create-{file_name}" - Creates the given file with an empty content. If the file already exists, remove the existing text in it (as if the file is created again)
+# •	"Add-{file_name}-{content}" - Append the content and a new line after it. If the file does not exist, create it, and add the content
+# •	"Replace-{file_name}-{old_string}-{new_string}" - Open the file and replace all the occurrences of the old string with the new string. If the file does not exist, print: "An error occurred"
+# •	"Delete-{file_name}" - Delete the file. If the file does not exist, print: "An error occurred"
+# Example
+# Input	Comment
+# Create-file.txt
+# Add-file.txt-First Line
+# Add-file.txt-Second Line
+# Replace-random.txt-Some-some
+# Replace-file.txt-First-1st
+# Replace-file.txt-Second-2nd
+# Delete-random.txt
+# Delete-file.txt
+# End	The first command creates the empty file
+# After the first and second Add command, the content is:
+# First Line
+# Second Line
+# On the first Replace command, an error must occur
+# After the next two Replace commands, the content is:
+# 1st Line
+# 2nd Line
+# After the first Delete command, an error occurs
+# Finally, the 'file.txt' file is deleted
