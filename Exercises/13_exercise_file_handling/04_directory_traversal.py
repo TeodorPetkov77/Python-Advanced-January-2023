af9b44152baa3868c8a33dae723d9c950f4b927e
RebelShark <tpetkov93@gmail.com>
2023-02-03 15:41:36 +0200

    x

--- Exercises/13_exercise_file_handling/04_directory_traversal.py
+++ Exercises/13_exercise_file_handling/04_directory_traversal.py
@@ -39,4 +39,19 @@ for extension, files in sorted(file_dict.items()):
 # The program is meant to work with the files in "04_directory_traversal_dir".
 
 
-#
+# 4.	Directory Traversal
+# Write a program that traverses a given directory for all files. Search through the first level of the directory only and write information about each found file in report.txt. The files should be grouped by their extension. Extensions should be ordered by name alphabetically. The files with extensions should also be sorted by name. report.txt should be saved in the chosen directory.
+# Examples
+# Input	Directory View	report.txt
+# .	 	.html
+# - - - index.html
+# .js
+# - - - index.js
+# .pptx
+# - - - demo.pptx
+# .py
+# - - - program.py
+# - - - python.py
+# .txt
+# - - - log.txt
+# - - - notes.txt
